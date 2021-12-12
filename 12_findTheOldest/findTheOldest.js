3bf73bd5dd51693570520a4e9db6e0a469447942
Sam H <shung93@uw.edu>
2021-12-12 19:14:24 +0000

    Completed exercises 8 thru 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currYear = new Date().getFullYear()
 
+    peopleArray.map(people => { if (people['yearOfDeath'] === undefined ) {
+                                    people['yearOfDeath'] = currYear}
+                            }
+                    );
+
+    peopleArray.map(people => people['age'] = (people['yearOfDeath'] - people['yearOfBirth']));
+
+    const oldestAge = Math.max(...peopleArray.map(people => people['age']))
+
+    const oldFolks = peopleArray.filter(people => people['age'] === oldestAge)
+    
+    for (let i = 0; i <= oldFolks.length; i++) {
+        return oldFolks[i]
+    }
 };
 
 // Do not edit below this line
